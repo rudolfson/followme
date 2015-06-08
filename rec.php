afe983f4427227afd3574f305cdad74e69896ba3
rudolfson <l.heller@web.de>
2015-06-09 01:00:32 +0200

    * simple json recording * displaying a map

--- rec.php
+++ rec.php
@@ -1,8 +1,27 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-  </body>
-</html>
+<?php
+header('Content-Type: text/plain');
+
+$name = trim($_REQUEST['name']);
+if ($name == '') $name = "default";
+
+$lat = trim($_REQUEST['lat']);
+$lon = trim($_REQUEST['lon']);
+$time = trim($_REQUEST['time']);
+$alt = trim($_REQUEST['alt']);
+$speed = trim($_REQUEST['speed']);
+
+$json = <<<EOT
+{
+    "lat": $lat,
+    "lon": $lon,
+    "time": "$time",
+    "alt": $alt,
+    "speed": $speed
+}
+EOT;
+
+$out = fopen("recording/$name", "w");
+fwrite($out, $json);
+fclose($out);
+
+?>
